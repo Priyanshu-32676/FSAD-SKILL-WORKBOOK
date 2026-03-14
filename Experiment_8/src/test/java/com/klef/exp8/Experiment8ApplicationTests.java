d15f9302719f69e1327a86a2b98602a0bbee4dbf
Priyanshu-32676 <2400032676@kluniversity.in>
2026-03-14 15:58:44 +0530

    Added Experiment_8

---
+++ Experiment_8/src/test/java/com/klef/exp8/Experiment8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klef.exp8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Experiment8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
